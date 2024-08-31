58a8e42d7094cbe022cadda1c62bc46320250fe5
Leonardo Flores <florees.leo@gmail.com>
2024-08-30 19:03:47 -0600

    🫧 feat: Tailwindcss Animation
    Se importa la biblioteca de Tailwindcss animation y se usa en la clase del h1 para comprobar funcionamiento.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@ import React from "react";
 export default function Home() {
   return (
     <div className="p-4 pt-2">
-      <h1 className="text-3xl font-black">Portafolio</h1>
+      <h1 className="animate-wiggle animate-infinite text-3xl font-black">
+        Portafolio
+      </h1>
       <h4 className="text-xl font-medium">Subtitulo</h4>
       <p className="text-sm font-light">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, ea!
