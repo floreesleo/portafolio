7117c5b6b82c814b613d00b1f086023d828e5b01
Leonardo Flores <florees.leo@gmail.com>
2024-08-31 13:17:19 -0600

    ⚠️ test: react-simple-typewriter
    Se importa la biblioteca de react-simple-typewriter para poder hacer el efecto de escribir texto.
    
    Se hacen cambios en el Header para que el usuario no pueda seleccionar el texto y se cambia el contenido del Titulo y de mi nombre.

--- src/components/single-use/Header.tsx
+++ src/components/single-use/Header.tsx
@@ -1,16 +1,14 @@
-import React from "react";
-
 export default function Header() {
   return (
-    <div>
+    <div className="select-none">
       {/* Titulo */}
       <h1 className="animate-flip-down text-5xl font-black animate-delay-[1500ms] animate-duration-[1200ms] animate-once">
-        leo<span className="text-violet-800">.dev()</span>
+        azael<span className="text-violet-800">.dev()</span>
       </h1>
 
       {/* Nombre */}
       <h4 className="mt-2 animate-fade-right text-2xl font-medium text-violet-800 animate-delay-[2400ms] animate-duration-1000 animate-once">
-        Leonardo Flores
+        Leonardo Azael
       </h4>
 
       {/* breve descripcion */}
