b0d49b5c622b8872ae22ee49b2a1de036c063c2d
Leonardo Flores <florees.leo@gmail.com>
2024-08-30 18:48:24 -0600

    ⌨️ feat: Roboto Mono font
    Se importa Roboto Mono de las fuentes de google font que proporciona nextjs.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Roboto_Mono } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const roboto_mono = Roboto_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={roboto_mono.className}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
 import React from "react";
 
 export default function Home() {
-  return <div className="p-4 pt-2">Portafolio</div>;
+  return (
+    <div className="p-4 pt-2">
+      <h1 className="text-3xl font-black">Portafolio</h1>
+      <h4 className="text-xl font-medium">Subtitulo</h4>
+      <p className="text-sm font-light">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, ea!
+      </p>
+    </div>
+  );
 }
