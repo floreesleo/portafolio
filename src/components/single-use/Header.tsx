a6fd4b154ee14188b32e31a51458635dd10f8026
Leonardo Flores <florees.leo@gmail.com>
2024-08-31 11:58:38 -0600

    🎨 style: Delay en animación
    Cambio de delay de animaciones al aparecer los textos del encabezado.

--- src/components/single-use/Header.tsx
+++ src/components/single-use/Header.tsx
@@ -4,17 +4,17 @@ export default function Header() {
   return (
     <div>
       {/* Titulo */}
-      <h1 className="animate-flip-down text-5xl font-black animate-delay-700 animate-duration-[2000ms] animate-once">
+      <h1 className="animate-flip-down text-5xl font-black animate-delay-[1500ms] animate-duration-[1200ms] animate-once">
         leo<span className="text-violet-800">.dev()</span>
       </h1>
 
       {/* Nombre */}
-      <h4 className="mt-2 animate-fade-right text-2xl font-medium text-violet-800 animate-delay-[3000ms] animate-duration-1000 animate-once">
+      <h4 className="mt-2 animate-fade-right text-2xl font-medium text-violet-800 animate-delay-[2400ms] animate-duration-1000 animate-once">
         Leonardo Flores
       </h4>
 
       {/* breve descripcion */}
-      <p className="mt-4 animate-fade-right text-xl font-light animate-delay-[3000ms] animate-duration-1000 animate-once">
+      <p className="mt-4 animate-fade-right text-xl font-light animate-delay-[2400ms] animate-duration-1000 animate-once">
         Creative frontend developer
       </p>
     </div>
