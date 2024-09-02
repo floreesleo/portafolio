b62a0488f7caef24bbbddb5eee6c1dfa2dc65424
Leonardo Flores <florees.leo@gmail.com>
2024-09-02 12:27:24 -0600

    ✨ feat: npm i lucide-react
    Instalación de la biblioteca Luide.dev para utilizar iconos en el proyecto, haciendo uso de estos en el componente de Navegación y en el componente de mi descripción.
    
    Se agregaron estilos en el global css para que al seleccionar texto tenga un background color violeta y el texto sea de color blanco.
    
    Se añadió mi cv a la carpeta public para descargar.

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { Github, Linkedin } from "lucide-react";
 
 export default function Nav() {
   const pathName = usePathname();
@@ -21,20 +22,52 @@ export default function Nav() {
       <div className="container mx-auto flex animate-fade-down items-center justify-between p-4 font-medium animate-duration-1000 animate-once">
         <h2 className="text-2xl font-black">{title}</h2>
 
-        <div className="flex flex-row space-x-4">
+        <div className="flex flex-row items-center space-x-4">
           <Link
             href={"/"}
-            className={pathName == "/" ? "text-neutral-500" : "text-white"}
+            className={
+              pathName == "/"
+                ? "text-neutral-500 transition-colors duration-500 hover:text-violet-800"
+                : "text-white transition-colors duration-500 hover:text-violet-800"
+            }
           >
-            .is()
+            .dev()
           </Link>
           <Link
             href={"/about"}
-            className={pathName == "/about" ? "text-neutral-500" : "text-white"}
+            className={
+              pathName == "/about"
+                ? "text-neutral-500 transition-colors duration-500 hover:text-violet-800"
+                : "text-white transition-colors duration-500 hover:text-violet-800"
+            }
           >
             .about()
           </Link>
-          <Link href={"mailto:florees.leo@gmail.com"}>.email()</Link>
+          <Link
+            href={"mailto:florees.leo@gmail.com"}
+            className="transition-colors duration-500 hover:text-violet-800"
+          >
+            .email()
+          </Link>
+          <Link href="https://github.com/floreesleo" target="_blank">
+            <Github
+              className="text-violet-800 transition-colors duration-500 hover:text-white"
+              size={23}
+              absoluteStrokeWidth={false}
+              strokeWidth={2}
+            />
+          </Link>
+          <Link
+            href="https://www.linkedin.com/in/flores-leonardo"
+            target="_blank"
+          >
+            <Linkedin
+              className="text-violet-800 transition-colors duration-500 hover:text-white"
+              size={23}
+              absoluteStrokeWidth={false}
+              strokeWidth={2}
+            />
+          </Link>
         </div>
       </div>
     </nav>
--- src/components/single-use/MyDescription.tsx
+++ src/components/single-use/MyDescription.tsx
@@ -1,21 +1,41 @@
-import Image from "next/image";
-import vercel from "@/icons/vercel.svg";
+import Link from "next/link";
+import { Github, Linkedin, Download } from "lucide-react";
 
 export default function MyDescription() {
   return (
     <div className="flex flex-col items-start space-y-6 text-lg">
       <h1 className="animate-flip-down select-none text-5xl font-black animate-delay-[1500ms] animate-duration-[1200ms] animate-once">
-        about(<span className="text-violet-800">azael</span>)
+        .about(<span className="text-violet-800">azael</span>)
       </h1>
 
       {/* Este div solo se usa para dividir los elementos verticalmente y darle una animación en general al contenedor completo */}
       <div className="animate-fade-right space-y-6 animate-delay-[2400ms] animate-duration-1000 animate-once">
         {/* Enlaces a mis redes */}
         <div className="flex flex-row items-center space-x-3">
-          <Image src="next.svg" alt="nextjs icon" width={24} height={24} />
-          <Image src={vercel} alt="vercel icon" width={24} height={24} />
-          <span>Download my</span>
-          <span className="rounded-xl bg-violet-800 px-3 py-1">CV</span>
+          <Link href="https://github.com/floreesleo" target="_blank">
+            <Github
+              className="text-violet-800 transition-colors duration-500 hover:text-white"
+              size={23}
+            />
+          </Link>
+          <Link
+            href="https://www.linkedin.com/in/flores-leonardo"
+            target="_blank"
+          >
+            <Linkedin
+              className="text-violet-800 transition-colors duration-500 hover:text-white"
+              size={23}
+            />
+          </Link>
+          <span className="select-none">Download my</span>
+          <a
+            href="/leonardo.azael.cv.pdf"
+            download="leonardo.azael.cv.pdf"
+            className="flex cursor-pointer select-none items-center rounded-3xl bg-violet-800 px-3 py-1 text-black transition-colors duration-500 hover:bg-white"
+          >
+            {/* revisar como hacer para que se descargue el pdf de mi cv */}
+            CV <Download size={20} className="ml-2" strokeWidth={2} />
+          </a>
         </div>
 
         {/* Descripción */}
