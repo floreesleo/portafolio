e79355effd1b13e0d41f7561eb90f68ab0ce4715
Leonardo Flores <florees.leo@gmail.com>
2024-09-29 17:07:38 -0600

    feat: 🔃 Ajuste Footer
    Se ajusta la altura del contenedor del Footer para tener más espaciado.
    
    Se actualiza la versión del proyecto en el Footer y en package.json

--- src/components/nav/Footer.tsx
+++ src/components/nav/Footer.tsx
@@ -1,8 +1,8 @@
 export default function Footer() {
   return (
-    <div className="flex h-[250px] select-none flex-col items-center justify-center space-y-2 text-sm font-extralight lg:h-[100px]">
+    <div className="flex h-[35vh] select-none flex-col items-center justify-center space-y-2 text-sm font-extralight">
       <span>Made with 🤍 in 2024</span>
-      <span>Version 2.0.2</span>
+      <span>Version 2.1.1</span>
     </div>
   );
 }
